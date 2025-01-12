f779fa2275770deb3576142584d13da0334d966c
hridoyPRGMR <hridoymia.prgmr@gmail.com>
2025-01-12 19:44:36 +0600

    User-service: Get User details with Ratings from Rating-Service and Hotel of that Rating From Hotel-Service.

--- UserService/src/main/java/com/microservice/user/service/UserService/entities/Hotel.java
+++ UserService/src/main/java/com/microservice/user/service/UserService/entities/Hotel.java
@@ -1,7 +1,6 @@
 package com.microservice.user.service.UserService.entities;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,14 +9,10 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class Rating {
-
-	private String id;
-	private String userId;
-	private String hotelId;
-	private int rating;
-	private String feedback;
+public class Hotel {
 	
-	private Hotel hotel;
+	private String id;
+	private String name;
+	private String location;
+	private String about;
 }
--- UserService/src/main/java/com/microservice/user/service/UserService/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/microservice/user/service/UserService/services/impl/UserServiceImpl.java
@@ -1,14 +1,18 @@
 package com.microservice.user.service.UserService.services.impl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.microservice.user.service.UserService.entities.Hotel;
 import com.microservice.user.service.UserService.entities.Rating;
 import com.microservice.user.service.UserService.entities.User;
 import com.microservice.user.service.UserService.exception.ResourceNotFoundException;
@@ -45,8 +49,22 @@ public class UserServiceImpl implements UserService {
 	public User getUser(String userId) {
 		User user = userRep.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User not found with id: "+userId));
 		
-		ArrayList<Rating> ratings = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
-		user.setRatings(ratings);
+		Rating[] ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), Rating[].class);
+		List<Rating> ratings = Arrays.stream(ratingsOfUser).toList();
+		
+		List<Rating> ratingList = ratings.stream().map(rating->{
+		
+			ResponseEntity<Hotel> response = restTemplate.getForEntity("http://localhost:8082/hotels/"+rating.getHotelId(), Hotel.class);
+			Hotel hotel = response.getBody();	
+			
+//			logger.info("{}",response);
+			
+			rating.setHotel(hotel);
+			return rating;
+		})
+		.collect(Collectors.toList());
+		
+		user.setRatings(ratingList);
 		
 		return user;
 	}
